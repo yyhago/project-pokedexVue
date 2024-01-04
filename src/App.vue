8f381060e4fa7e2ac68b00a391c6a4c35d96c8a4
Yhago Felipe <yhago.felipe.teles@gmail.com>
2024-01-04 18:49:32 -0300

    Requisição HTTP

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,25 @@
   </div>
 </template>
 
-
 <script>
+import axios from "axios";
+
 export default {
-  
-}
+  name: "App",
+  data() {
+    return {
+      pokemons: [],
+    };
+  },
+  created: function () {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+      .then((resp) => {
+        console.log("Lista Pokemon Feita com Sucesso!");
+        this.pokemons = resp.data.results;
+      });
+  },
+};
 </script>
 
-
-<style scoped>
-
-</style>
+<style scoped></style>
